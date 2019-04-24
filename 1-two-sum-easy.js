e454f7525b457a097b8e786dc4cecfdec2fd4746
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2019-04-24 16:33:28 +0530

    Optimized code samples
    Optimized code samples

--- 1-two-sum-easy.js
+++ 1-two-sum-easy.js
@@ -4,6 +4,29 @@ You may assume that each input would have exactly one solution, and you may not 
 */
 
 function twoSum(nums, target) {
+  /* best space optimized - 33504 kb
+  **********************************
+  for (let i = 0; i < nums.length; i++) {
+    let currentNum = nums[i];
+    let difference = target - currentNum;
+
+    if (nums.includes(difference) && nums.indexOf(difference) != i) {
+      return [i, nums.indexOf(difference)];
+    }
+  }
+  */
+  
+  /* best time optimized - 56 ms
+  ******************************
+  const map = {};
+  for (let i=0; i<nums.length-1; i++) {
+    map[target-nums[i]] = i;
+    if (nums[i+1] in map) {
+      return [map[nums[i+1]], i+1];
+    }
+  }
+  */
+  
   var myMap = new Map(),
   		i = 0,
       j = 0;
